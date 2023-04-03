b8609423611604e85940f5eba6e42624b30fd55d
VladZhyganov <Wjigan@gmail.com>
2023-04-03 19:35:35 +0300

    modified Lection_1/Task_3/main.py

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,18 +1,18 @@
 print ("to calculate, enter the first number and press ENTER, then the action sign, press ENTER, and the second number, press ENTER")
-a= int(input ("enter first number_ "))
-b= (input ("enter action sign_ "))
-c= int(input ("enter second number_"))
-if b in ('+','-','/','*'):
-    if  b==  '+':
-        print  (a,"+",c,"=",a+c)
-    if  b== '-':
-        print  (a,"-",c,"=",a-c)
-    if  b== '*':
-        print  (a,"*",c,"=",a*c)
-    if  b== '/' :
-        if c ==0:
+first_number= int(input ("enter first number_ "))
+sign= (input ("enter action sign_ "))
+second_number= int(input ("enter second number_"))
+if sign in ('+','-','/','*'):
+    if  sign==  '+':
+        print  (first_number,"+",second_number,"=",first_number+second_number)
+    if  sign== '-':
+        print  (first_number,"-",second_number,"=",first_number-second_number)
+    if  sign== '*':
+        print  (first_number,"*",second_number,"=",first_number*second_number)
+    if  sign== '/' :
+        if second_number ==0:
             print ("division by 0 is prohibited!")
         else: 
-            print  (a,"/",c,"=",a/c)
+            print  (first_number,"/",second_number,"=",first_number/second_number)
 else: 
     print  ("incorrect action sign")
