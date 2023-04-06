8db92045195cbada32dd8f9b0f95e515f8da6ccf
VladZhyganov <Wjigan@gmail.com>
2023-04-06 19:51:16 +0300

    changed Lection_2/Task_1/main.py

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,50 +1,25 @@
-print ("enter 10 elements to form your list - these can be 'int, str, float, bool ")
 your_list = []
-x = 0
-while True:
-    input_element = input (("Enter element: "))
-    x +=1
-    if input_element.isdigit():
-        item = int(input_element)
-    elif "." in input_element:
+for i in range(5):
+    element = input(f"Enter element {i+1}: ")
+    if element.isdigit():
+        your_list.append(int(element))
+    elif "." in element:
         try:
-            item = float(input_element)
+            your_list.append(float(element))
         except ValueError:
-            item = input_element
-    elif input_element.lower() == "true" or input_element.lower() == "false":
-        item = bool(input_element)
+            your_list.append(element)
+    elif element.lower() == "true" or element.lower() == "false":
+        your_list.append(bool(element))
     else:
-        item = input_element
-    your_list.append(item)
-    
-    if x==10:
-        break
-    print("You entered", x, "element.")
-print("in you list entered", x, "elements.")
-print ("")
-print("The final list:", your_list)
+        your_list.append(element)
 
-print ()
+print(f"Your list contains {len(your_list)} elements: {your_list}")
+
 while True:
-    print ("to select and determine the number of elements by type from your list, enter the name of the type") 
-    a=input("enter 'int, str, float,bool, for exit enter 'quit'")
-    if a == "quit":
-        print ("Good bay!!!")
+    typ = input("Enter a data type ('int', 'str', 'float', 'bool') to select elements, or 'quit' to exit: ")
+    if typ == "quit":
+        print("Goodbye!")
         break
-    if a=='str':
-        type_to_select =str
-    if a=='bool':
-        type_to_select =bool
-    if a=='float':
-        type_to_select =float
-    if a=='int':
-        type_to_select =int
-
-    sel_elements = []
-
-    for element in your_list:
-        if type(element) == type_to_select:
-                sel_elements.append(element)
-
-    print("selected elements:", a, sel_elements)
-    print("how many elements:", len(sel_elements))    
+    selected_elements = [e for e in your_list if type(e).__name__ == typ]
+    print(f"Selected elements of type {typ}: {selected_elements}")
+    print(f"Number of elements of type {typ}: {len(selected_elements)}")
