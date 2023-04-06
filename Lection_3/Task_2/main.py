0a3297d0bf16d293024e64ba52de2cd9e761ea75
VladZhyganov <Wjigan@gmail.com>
2023-04-06 21:23:22 +0300

    add Lection_3/Task_2/main.py

---
+++ Lection_3/Task_2/main.py
@@ -0,0 +1,7 @@
+def drawrRomb(size=5):
+    for i in range(size):
+        print(" " * (size-i-1) + "* " * (i + 1))
+    for i in range(size-2, -1, -1):
+        print(" " * (size-i-1) + "* " * (i+1))
+
+drawrRomb(5)
